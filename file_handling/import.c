b6f8dfb1ce64b398fca44bc264c6be2d6cac3065
Keepersam02 <samuelnoblemedia@gmail.com>
2025-07-04 01:16:26 -0700

    Takes demosiaced images-> normalizes them -> stores them in an intermediate format, working lut application.
    WIP importing a directory

--- file_handling/import.c
+++ file_handling/import.c
@@ -4,8 +4,31 @@
 
 #include "import.h"
 #include "libraw/libraw.h"
+#include <sys/stat.h>
 #include <libraw/libraw_version.h>
 
+typedef enum {
+    DIRECTORY = 0,
+    REGULARFILE = 1,
+
+}fileTypes;
+
+int fileType(char* path) {
+    struct stat statbuf;
+    if (stat(path, &statbuf) != 0) {
+        return -1;
+    }
+    int res = S_ISDIR(statbuf.st_mode);
+    if (S_ISDIR(statbuf.st_mode) == 0) {
+        return DIRECTORY;
+    }
+    if (S_ISREG(statbuf.st_mode) == 0) {
+        return REGULARFILE;
+    }
+    return -2;
+}
+
+int openDir(char* path, )
 
 int openFile(char* path, libraw_data_t* rawReader) {
     int res = libraw_open_file(rawReader, path);
@@ -23,7 +46,7 @@ bool valid_image(char* path) {
     }
     printf("File exists\n");
 
-    uint8_t buffer[16];
+    char buffer[16];
     size_t bytesRead = fread(buffer, 1, 16, file);
     buffer[15] = '\0';
     fclose(file);
@@ -31,7 +54,7 @@ bool valid_image(char* path) {
         printf("Could not read image header");
         return 0;
     }
-    uint8_t magicNum[16] = {'F', 'U', 'J', 'I', 'F', 'I', 'L', 'M', 'C', 'C', 'D', '-', 'R', 'A', 'W', '\0'};
+    char magicNum[16] = {'F', 'U', 'J', 'I', 'F', 'I', 'L', 'M', 'C', 'C', 'D', '-', 'R', 'A', 'W', '\0'};
     printf("%s\n", buffer);
     printf("%s\n", magicNum);
     for (int i = 0;i < 15;i++) {
@@ -63,3 +86,39 @@ int ReadImageData16(libraw_processed_image_t** img, libraw_data_t* rawProc) {
     *img = libraw_dcraw_make_mem_image(rawProc, &res);
     return 0;
 }
+
+int createIntermediateImage(libraw_processed_image_t* img, void** interImP) {
+    if (!img) {
+        return -1;
+    }
+    intermediateImage* interIm = malloc(sizeof(intermediateImage));
+    interIm->bits = img->bits;
+    interIm->colors = img->colors;
+    interIm->height = img->height;
+    interIm->width = img->width;
+    interIm->dataSize = img->data_size;
+    interIm->gamma = 1;
+    double* normData = malloc(sizeof(double) * interIm->dataSize);
+    int res = normalizeImage(img->data, img->data_size, normData);
+    if (res != 0) {
+        free(normData);
+        free(interIm);
+        return -1;
+    }
+    interIm->data = normData;
+    *interImP = interIm;
+    return 0;
+}
+
+int normalizeImage(char* dataIn, unsigned int size, double* normIm) {
+    if (!dataIn || !normIm) {
+        return -1;
+    }
+    uint16_t cur;
+    for (int i = 0; i < size; i++) {
+        memcpy(&cur, dataIn + (i * 2), sizeof(uint16_t));
+        *(normIm + i) = (double)cur / 65535;
+    }
+    return 0;
+}
+
--- lut-handle/lutApp.h
+++ lut-handle/lutApp.h
@@ -5,4 +5,8 @@
 #ifndef LUTAPP_H
 #define LUTAPP_H
 
+double clipCheck(double in);
+int applyPix65(double in[3], double lutDat[274625]);
+int applyIm65(double* data, int size, double lutDat[274625]);
+
 #endif //LUTAPP_H
