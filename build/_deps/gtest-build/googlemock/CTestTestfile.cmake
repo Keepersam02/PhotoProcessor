17df79648d8db7c4d1168111621627a55373d3e3
Keepersam02 <samuelnoblemedia@gmail.com>
2025-12-25 00:53:46 -0800

    removed build folder

--- build/_deps/gtest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/keepersam02/coding_projects/02-personal/batch_photo_processor/build/_deps/gtest-src/googlemock
-# Build directory: /home/keepersam02/coding_projects/02-personal/batch_photo_processor/build/_deps/gtest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- src/io/image_io.cpp
+++ src/io/image_io.cpp
@@ -24,13 +24,28 @@ bool valid_file(std::string path) {
   }
   i_file.close();
 
-  std::array<std::byte, 10> header_10{
+  std::array<std::byte, 10> cr2{
       std::byte(0x49), std::byte(0x49), std::byte(0x2A), std::byte(0x00),
       std::byte(0x10), std::byte(0x00), std::byte(0x00), std::byte(0x00),
       std::byte(0x43), std::byte(0x52)};
-  std::array<std::byte, 16> header_16{
+  std::array<std::byte, 16> fraw{
       std::byte{'F'}, std::byte{'U'}, std::byte{'J'}, std::byte{'I'},
       std::byte{'F'}, std::byte{'I'}, std::byte{'L'}, std::byte{'M'},
       std::byte{'C'}, std::byte{'C'}, std::byte{'D'}, std::byte{'-'},
       std::byte{'R'}, std::byte{'A'}, std::byte{'W'}};
+
+  std::array<std::array<std::byte, 10>, 1> length_10{cr2};
+  std::array<std::array<std::byte, 16>, 1> length_16{fraw};
+
+  for (std::array<std::byte, 10> cur : length_10) {
+    if (std::equal(magic_num.begin(), magic_num.begin() + 10, cur.begin())) {
+      return true;
+    }
+  }
+  for (std::array<std::byte, 16> cur : length_16) {
+    if (std::equal(magic_num.begin(), magic_num.end(), cur.begin())) {
+      return true;
+    }
+  }
+  return false;
 }
