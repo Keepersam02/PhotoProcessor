1b5fcae0dc7d161ac273a15db48feb583203aaeb
Keepersam02 <samuelnoblemedia@gmail.com>
2026-01-04 11:22:41 -0800

    WIP image io

--- src/io/image_io.cpp
+++ src/io/image_io.cpp
@@ -1,51 +1,57 @@
+#include "image_io.hpp"
 #include "libraw/libraw.h"
 #include <array>
 #include <cstddef>
 #include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <libraw/libraw_const.h>
+#include <memory>
+#include <png.h>
 #include <string>
+#include <tiffio.h>
 
 namespace fs = std::filesystem;
 
-bool valid_file(std::string path) {
+image_type valid_file(std::string path) {
   fs::path file_path(path);
-  if (!fs::exists(file_path)) {
-    return false;
+  if (!fs::exists(file_path) || !fs::is_regular_file(path)) {
+    return FILE_DOES_NOT_EXIST;
+  }
+  return FILE_EXIST;
+}
+
+image_type supported_image(std::string path) {
+  LibRaw raw;
+  if (raw.open_file(path.c_str()) != LIBRAW_SUCCESS) {
+    return RAW_FILE;
+  }
+  raw.recycle();
+
+  std::ifstream file(path);
+  std::array<std::byte, 16> header{};
+  file.read(reinterpret_cast<char *>(header.data()), header.size());
+  if (file.gcount() != 16) {
+    std::cerr << "failed to read expected number of bytes from: " << path
+              << " read: " << file.gcount() << std::endl;
+    file.close();
   }
 
-  std::array<std::byte, 16> magic_num{};
-  std::ifstream i_file(file_path, std::ios::binary);
-  i_file.read(reinterpret_cast<char *>(magic_num.data()), magic_num.size());
-  if (i_file.gcount() != 16) {
-    std::cerr << "failed to read 16 bytes from " << file_path.filename()
-              << " read " << i_file.gcount() << std::endl;
-    i_file.close();
-    return false;
-  }
-  i_file.close();
+  std::array<std::byte, 8> png_header{
+      std::byte(137), std::byte(80), std::byte(78), std::byte(71),
+      std::byte(13),  std::byte(10), std::byte(26), std::byte(10)};
+  std::array<std::byte, 4> tiff_header{std::byte(0x49), std::byte(0x49),
+                                       std::byte(0x2A), std::byte(0x00)};
 
-  std::array<std::byte, 10> cr2{
-      std::byte(0x49), std::byte(0x49), std::byte(0x2A), std::byte(0x00),
-      std::byte(0x10), std::byte(0x00), std::byte(0x00), std::byte(0x00),
-      std::byte(0x43), std::byte(0x52)};
-  std::array<std::byte, 16> fraw{
-      std::byte{'F'}, std::byte{'U'}, std::byte{'J'}, std::byte{'I'},
-      std::byte{'F'}, std::byte{'I'}, std::byte{'L'}, std::byte{'M'},
-      std::byte{'C'}, std::byte{'C'}, std::byte{'D'}, std::byte{'-'},
-      std::byte{'R'}, std::byte{'A'}, std::byte{'W'}};
-
-  std::array<std::array<std::byte, 10>, 1> length_10{cr2};
-  std::array<std::array<std::byte, 16>, 1> length_16{fraw};
-
-  for (std::array<std::byte, 10> cur : length_10) {
-    if (std::equal(magic_num.begin(), magic_num.begin() + 10, cur.begin())) {
-      return true;
-    }
+  bool is_png =
+      std::equal(png_header.begin(), png_header.end(), header.begin());
+  bool is_tiff =
+      std::equal(tiff_header.begin(), tiff_header.end(), header.begin());
+  if (is_png) {
+    return PNG_FILE;
   }
-  for (std::array<std::byte, 16> cur : length_16) {
-    if (std::equal(magic_num.begin(), magic_num.end(), cur.begin())) {
-      return true;
-    }
+  if (is_tiff) {
+    return TIFF_FILE;
   }
-  return false;
+  return UNSUPPORTED_IMAGE;
 }
--- src/io/image_io.hpp
+++ src/io/image_io.hpp
@@ -1,1 +1,17 @@
+#pragma once
 
+#include <string>
+
+enum image_type {
+  RAW_FILE,
+  TIFF_FILE,
+  PNG_FILE,
+  JPEG_FILE,
+  UNSUPPORTED_IMAGE,
+  NOT_IMAGE,
+  FILE_DOES_NOT_EXIST,
+  FILE_EXIST
+};
+
+image_type valid_file(std::string path);
+image_type supported_image(std::string path);
