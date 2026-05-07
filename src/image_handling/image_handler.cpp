b4abbe389817a4074bd683ce1b0a99bd56f60463
Keepersam02 <samuelnoblemedia@gmail.com>
2026-05-07 13:46:10 -0700

    storing for syncthing sync

--- src/image_handling/image_handler.cpp
+++ src/image_handling/image_handler.cpp
@@ -1,107 +1,39 @@
-#include "convert.hpp"
-#include "image.hpp"
-#include "io/image_io.hpp"
 #include "io/image_io_error.hpp"
-#include "libraw.h"
-#include "types.hpp"
-#include "types/image.hpp"
-#include <cstddef>
-#include <cstdint>
 #include <expected>
+#include <filesystem>
+#include <format>
 #include <utility>
 #include <vector>
 
 namespace fs = std::filesystem;
 
-/*
- * General image handler.  Takes a set of file poths and a pipeline and executes
- * the pipeline on the imaeges.
- *   - determine file type
- *   - load into memory
- *   - debayer, pre-debayered support later
- *   - execute pipeline
- *   - pass to appropriate export controller
- */
-std::expected<std::vector<std_image>, image_error>
-handler(std::vector<fs::path> file_paths, std::vector<int *> pipeline,
-        fs::path output_dir) {
-  std::vector<std::pair<fs::path, image_error>> failed;
-  auto in_mem_files = file_loader(file_paths, failed);
-  if (!in_mem_files) {
-    // todo
+std::expected<bool, image_error> file_copier(void *file, size_t file_size,
+                                             fs::path src, fs::path dest_dir) {
+  const auto file_name = src.filename();
+  fs::path file_dest = dest_dir / file_name;
+
+  auto dest_file = fopen(file_dest.c_str(), "wb");
+  if (dest_file == NULL) {
+    return image_error::IO(-1, err_severity::ERROR,
+                           "failed to create destination file to write to",
+                           std::format("src path:{}, dest path: {}",
+                                       src.c_str(), file_dest.c_str()));
   }
-  if (failed.size() != 0) {
+  auto written = fwrite(file, file_size, 1, dest_file);
+  if (written == 0) {
+    return image_error::IO(
+        -1, err_severity::ERROR, "failed to write to destination",
+        std::format("src:{}, dest:{}", src.c_str(), file_dest.c_str()));
   }
-  failed.clear();
-  auto &images = image_creator(in_mem_files, failed);
-  if (!images) {
-    // todo
-  }
-  for (auto &image : images) {
-    for (const auto func : pipeline) {
-      auto ret = func(image);
-      if (!ret) {
-        // todo
-      }
-      image = ret;
-    }
-  }
-  return images;
+  return true;
 }
 
-std::expected<std::vector<std_image>, image_error>
-image_creator(std::vector<std::pair<void *, size_t>> files,
-              std::vector<std::pair<char *, image_error>> failed) {
-  std::vector<std::tuple<std::vector<flaot>, Exiv2::ExifData, Exiv2::XmpData>>
-      images;
-  for (const auto &file : files) {
-    auto image_dat = get_image_data(file.first, file.second);
-    if (!image_dat) {
-      return image_error::IO(0, err_severity::ERROR,
-                             "failed to get image data from file",
-                             "fail to debayer")
-    }
-    auto exif_data = get_exif_data(file.first, file.second);
-    if (!exif_data) {
-      image_error::IO(0, err_severity::ERROR,
-                      "failed to retrieve data from file",
-                      "exif data retrieval faileure");
-    }
-    images.push_back({image_dat, exif_data.first, exif_data.second})
+std::vector<fs::path>
+file_copy_ver(std::vector<std::pair<long, fs::path>> files) {
+  for (const auto file : files) {
   }
-  return images;
 }
 
-std::expected<std::vector<float>, image_error> get_image_data(const void *file,
-                                                              size_t size) {
-  LibRaw i_proc;
-  i_proc.open_buffer(file.first, file.second);
-  i_proc.unpack();
-  i_proc.imgdata.params.output_bps = 16;
-  i_proc.imgdata.params.output_color = 8;
-  i_proc.imgdata.params.gamm[0] = 1.0;
-  i_proc.imgdata.params.gamm[1] = 1.0;
-  i_proc.imgdata.params.no_auto_bright = 1;
-  i_proc.dcraw_process();
-  std::vector<float> image_data;
-  auto *data = reinterpret_cast<uint16_t *>(i_proc.imgdata.image);
-  for (int i = 0;
-       i < i_proc.imgdata.sizes.height * i_proc.imgdata.sizes.width * 4;
-       i += 4) {
-    image_data.push_back(data[i] / 65535);
-    image_data.push_back(data[i + 1] / 65535);
-    image_data.push_back(data[i + 2] / 65535);
-  }
-  return image_data;
-}
-
-std::expected<std::pair<Exiv2::ExifData, Exiv2::XmpData>, image_error>
-get_exif_data(void *file, size_t size) {
-  auto *file_p = static_cast<const Exiv2::byte *>(file);
-  auto exif_p = Exiv2::ImageFactory::open(file_p, size);
-  exif_p->readMetatdata();
-
-  auto const exif_meta_data = exif_p->exifData();
-  auto const xmp_data = exif_p->expData();
-  return {exif_meta_data, xmp_data};
+std::expected<long, image_error> file_hash(void *file) {
+  // use openssl to hash the source file dest
 }
--- src/image_handling/image_handler.hpp
+++ src/image_handling/image_handler.hpp
@@ -1,17 +1,7 @@
 #include "io/image_io_error.hpp"
-#include "types/image.hpp"
 #include <expected>
 #include <filesystem>
-#include <vector>
-
 namespace fs = std::filesystem;
 
-std::expected<std::vector<std_image>, image_error>
-handler(std::vector<fs::path> file_paths, std::vector<int *> pipeline);
-
-std::expected<std::vector<std_image>, image_error>
-image_creator(std::vector<std::pair<void *, size_t>> files,
-              std::vector<std::pair<char *, image_error>> failed);
-
-std::expected<std::pair<Exiv2::ExifData, Exiv2::XmpData>, image_error>
-get_exif_data(void *file, size_t size);
+std::expected<bool, image_error> file_copier(void *file, size_t file_size,
+                                             fs::path src, fs::path dest_dir);
